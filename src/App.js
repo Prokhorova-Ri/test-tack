6e871792b9ac572fb4622ae24c06eccd8dcf347f
Prokhorova_Ri <rena2652@gmail.com>
2023-01-22 15:11:44 +0300

    1) Создание сохранение массива, отображение в alert. 2) Заготовка кнопки и поля для загрузки массива.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,35 @@ import { useState } from "react";
 
 function App() {
 
-    const [cars, useCars] = useState([
+    const [cars, setCars] = useState([
         { name: 'Марка', value: 'Мазда'},
         { name: 'Модель', value: '6'},
         { name: 'Год выпуска', value: '2020'},
         { name: 'Количество', value: '6'},
     ])
 
+    const setNewValues = (payload) => {
+        const { event, index } = payload
+        let newValue = []
+        newValue.push(...cars)
+        newValue[index].value = event.target.value
+        setCars(newValue)
+    }
+
     const getLayoutValues = cars.map((item, index) => {
         return (
             <div className="items" key={ index }>
                 <p className="items-title">{ item.name }</p>
-                <input type="text" defaultValue={ item.value }/>
+                <input type="text"
+                       defaultValue={ item.value }
+                       onChange={(event) => setNewValues({ event, index })}/>
             </div>
         )
     })
+
+    const saveFile = () => {
+        alert(`Вы сохранили файл с данными \n: ${JSON.stringify(cars)}`);
+    }
 
     return (
         <div className="App">
@@ -26,8 +40,11 @@ function App() {
             </div>
 
             <div className="entry-field">
-                <textarea name="" id="" cols="1" rows="1"></textarea>
-                <button>Загрузить</button>
+                <div className="entry-field-upload">
+                    <textarea className="entry-field-upload-textarea" cols="1" rows="1" defaultValue='' />
+                    <button className="entry-field-upload-btn">Загрузить</button>
+                </div>
+                <button className="entry-field-download" onClick={saveFile}>Сохранить</button>
             </div>
         </div>
     );
